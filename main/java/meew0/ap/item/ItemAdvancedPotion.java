d30fc2cf9eb73e3b650e68c6cc472e31321d9366
meew0 <blactbt@live.de>
2014-03-22 09:48:05 +0100

    Added information to potion item

--- main/java/meew0/ap/item/ItemAdvancedPotion.java
+++ main/java/meew0/ap/item/ItemAdvancedPotion.java
@@ -2,15 +2,21 @@ package meew0.ap.item;
 
 import meew0.ap.AdvancedPotions;
 import meew0.ap.backend.EffectWrapper;
+import meew0.ap.backend.IPotionEffectContainer;
+import meew0.ap.backend.PotionRegistry;
 import meew0.ap.te.TileEntityAdvancedCauldron;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by meew0 on 21.03.14.
@@ -48,5 +54,24 @@ public class ItemAdvancedPotion extends Item {
     public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
         player.setItemInUse(stack, this.getMaxItemUseDuration(stack));
         return stack;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean flag) {
+        if (stack.stackTagCompound != null) {
+
+            list.add(EnumChatFormatting.ITALIC.toString() + EnumChatFormatting.GRAY.toString() + "Balance: " + stack.stackTagCompound.getInteger("bal"));
+            list.add(EnumChatFormatting.ITALIC.toString() + EnumChatFormatting.GRAY.toString() + "Balance modifier: " + stack.stackTagCompound.getInteger("balmod"));
+            list.add(EnumChatFormatting.ITALIC.toString() + EnumChatFormatting.GRAY.toString() + "Effects: ");
+
+            ArrayList<EffectWrapper> effects = new ArrayList<EffectWrapper>();
+            effects.addAll(TileEntityAdvancedCauldron.getEffectWrappersForTileNBT(stack.stackTagCompound));
+            for (EffectWrapper effect : effects) {
+                IPotionEffectContainer e = effect.getEffect();
+                list.add(EnumChatFormatting.ITALIC.toString() + EnumChatFormatting.BLUE + e.effectPrefix() + StatCollector.translateToLocal(e.effectName()) +
+                        ((e.displayAmplifier()) ? (" " + PotionRegistry.getRomanNumeral(e.getAmplifierForDisplay())) : "") + " (" +
+                        ((e.displayDuration()) ? ("" + PotionRegistry.getReadableDuration(e.getDurationForDisplay())) : "") + ")");
+            }
+        }
     }
 }
