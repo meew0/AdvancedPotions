c3e2d89b2a75c55093c09cee2665e42a44806857
meew0 <blactbt@live.de>
2014-05-12 16:42:10 +0200

    Beacons, try 2. Nothing works yet

--- main/java/meew0/ap/block/BlockAdvancedBeacon.java
+++ main/java/meew0/ap/block/BlockAdvancedBeacon.java
@@ -1,9 +1,31 @@
 package meew0.ap.block;
 
+import meew0.ap.AdvancedPotions;
+import meew0.ap.te.TileEntityAdvancedBeacon;
 import net.minecraft.block.BlockBeacon;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 /**
  * Created by meew0 on 30.04.14.
  */
 public class BlockAdvancedBeacon extends BlockBeacon {
+
+    @Override
+    public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_) {
+        return new TileEntityAdvancedBeacon();
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int m, float dx, float dy, float dz) {
+        if (!world.isRemote) {
+            TileEntityAdvancedBeacon te = (TileEntityAdvancedBeacon) world.getTileEntity(x, y, z);
+
+            if (te != null) {
+                player.openGui(AdvancedPotions.instance, 0, world, x, y, z);
+            }
+        }
+        return true;
+    }
 }
--- main/java/meew0/ap/te/TileEntityAdvancedBeacon.java
+++ main/java/meew0/ap/te/TileEntityAdvancedBeacon.java
@@ -1,9 +1,84 @@
 package meew0.ap.te;
 
+import meew0.ap.item.ItemAdvancedPotion;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntityBeacon;
 
 /**
  * Created by meew0 on 30.04.14.
  */
 public class TileEntityAdvancedBeacon extends TileEntityBeacon {
+    public ItemStack potionStack;
+
+    public TileEntityAdvancedBeacon() {
+        potionStack = null;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+        potionStack = ItemStack.loadItemStackFromNBT(nbt.getCompoundTag("PotionStack"));
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbt) {
+        super.writeToNBT(nbt);
+
+        if (potionStack != null) {
+            NBTTagCompound tag = new NBTTagCompound();
+            potionStack.writeToNBT(tag);
+            nbt.setTag("PotionStack", tag);
+        }
+    }
+
+    @Override
+    public int getSizeInventory() {
+        return 2;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int par1) {
+        return (par1 != 1) ? super.getStackInSlot(par1) : potionStack;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int par1, int par2) {
+        if (par1 == 1 && this.potionStack != null) {
+            if (par2 >= this.potionStack.stackSize) {
+                ItemStack itemstack = this.potionStack;
+                this.potionStack = null;
+                return itemstack;
+            } else {
+                this.potionStack.stackSize -= par2;
+                return new ItemStack(this.potionStack.getItem(), par2, this.potionStack.getItemDamage());
+            }
+        } else return super.decrStackSize(par1, par2);
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int par1) {
+        if (par1 == 1 && this.potionStack != null) {
+            ItemStack itemstack = this.potionStack;
+            this.potionStack = null;
+            return itemstack;
+        } else return super.getStackInSlotOnClosing(par1);
+    }
+
+    @Override
+    public void setInventorySlotContents(int par1, ItemStack par2ItemStack) {
+        if (par1 == 1) potionStack = par2ItemStack;
+        else super.setInventorySlotContents(par1, par2ItemStack);
+    }
+
+    @Override
+    public String getInventoryName() {
+        return hasCustomInventoryName() ? super.getInventoryName() : "container.advancedBeacon";
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int par1, ItemStack par2ItemStack) {
+        if (par1 == 1) return par2ItemStack.getItem() instanceof ItemAdvancedPotion;
+        else return super.isItemValidForSlot(par1, par2ItemStack);
+    }
 }
